5fc3aaef8118239661f5e6a6b6166a689a29bf9e
Mohd Farhan Akhtar <akhtar.farhan779@gmail.com>
2022-03-10 19:59:47 +0530

    redo sidebar

--- src/Components/Wrapper/Wrapper-components/Sidebar.js
+++ src/Components/Wrapper/Wrapper-components/Sidebar.js
@@ -9,50 +9,58 @@ import { VscSettingsGear as SettingsIcon } from 'react-icons/vsc';
 import { Link } from 'react-router-dom';
 
 const Sidebar = () => {
-    const activeborderclass = `border-l-2 border-l-SideBarTextActive w-full flex justify-center py-2`;
+    const activeborderclass = `border-l-2 border-l-SideBarTextActive w-full h-full flex justify-center p-2`;
 
 
     return (
-        <div className='flex flex-col justify-between w-12 h-full bg-NavbarBg py-3'>
+        <div className='flex flex-col justify-between h-full bg-NavbarBg border-b-2 border-gray-900'>
 
             {/* upar ke 5 icons */ }
-            <div>
+            {/* <div>
                 <ul className='flex flex-col items-center'>
                     <li className={ `${activeborderclass}` }>
                         <Link to='/home'>
                             <FilesIcon className='text-2xl text-SideBarTextActive hover:text-SideBarTextHover' />
                         </Link>
                     </li>
-                    <li className='w-full flex justify-center py-2'>
+                    <li className='w-full flex justify-center p-2'>
                         <Link to='/contact'>
                             <SearchIcon className='text-2xl text-SideBarText hover:text-SideBarTextHover' />
                         </Link>
                     </li>
-                    <li className='w-full flex justify-center py-2'>
+                    <li className='w-full flex justify-center p-2'>
                         <Link to='/github'>
                             <GitBranchIcon className='text-2xl text-SideBarText hover:text-SideBarTextHover' />
                         </Link>
                     </li>
-                    <li className='w-full flex justify-center py-2'>
+                    <li className='w-full flex justify-center p-2'>
                         <Link to='/projects'>
                             <ProjectsIcon className='text-2xl text-SideBarText hover:text-SideBarTextHover' />
                         </Link>
                     </li>
-                    <li className='w-full flex justify-center py-2'>
+                    <li className='w-full flex justify-center p-2'>
                         <Link to='/about'>
                             <FilePdfIcon className='text-2xl text-SideBarText hover:text-SideBarTextHover' />
                         </Link>
                     </li>
                 </ul>
-            </div>
+            </div> */}
 
             {/* neeche ke 2 icons */ }
-            <div>
-                <ul className='flex flex-col items-center gap-4'>
-                    <li><Link to='/about'><UserIcon className='text-2xl text-SideBarText hover:text-SideBarTextHover' /></Link></li>
-                    <li><Link to='/about'><SettingsIcon className='text-2xl text-SideBarText hover:text-SideBarTextHover' /></Link></li>
+            {/* <div>
+                <ul className='flex flex-col items-center'>
+                    <li className='w-full flex justify-center py-2'>
+                        <Link to='/about' className='text-2xl text-SideBarText hover:text-SideBarTextHover'>
+                            <UserIcon />
+                        </Link>
+                    </li>
+                    <li className='w-full flex justify-center '>
+                        <Link to='/about' className='text-2xl text-SideBarText hover:text-SideBarTextHover'>
+                            <SettingsIcon />
+                        </Link>
+                    </li>
                 </ul>
-            </div>
+            </div> */}
         </div >
     )
 };
